bbb981696c2ec6adf18b58f0a70d695be2f755a6
Lorenzo Travagliati <lorenzo.travagliati@gmail.com>
2019-11-04 16:06:45 +0100

    Refactored customer model
    Splitted customer model in input and output model to avoid showing PIN
    secret when performing GET operations.

--- obar/apis/customer_namespace.py
+++ obar/apis/customer_namespace.py
@@ -8,7 +8,7 @@ from obar import db
 
 customer_ns = Namespace('customer', description='Customer related operations')
 
-model = customer_ns.model('Customer', {
+customer_input_model = customer_ns.model('Customer Input', {
     'mail_address': fields.String(required=True,
                                   description='The customer mail address',
                                   attribute='customer_mail_address'),
@@ -23,10 +23,17 @@ model = customer_ns.model('Customer', {
                                attribute='customer_last_name')
 })
 
-
-@customer_ns.errorhandler(IntegrityError)
-def handle_operational_exception(self):
-    return {'message': 'What you want'}, 400
+customer_output_model = customer_ns.model('Customer Output', {
+    'mail_address': fields.String(required=True,
+                                  description='The customer mail address',
+                                  attribute='customer_mail_address'),
+    'first_name': fields.String(required=True,
+                                description='The customer first name',
+                                attribute='customer_first_name'),
+    'last_name': fields.String(required=True,
+                               description='The customer last name',
+                               attribute='customer_last_name')
+})
 
 
 @customer_ns.route('/')
@@ -34,7 +41,7 @@ class CustomerListAPI(Resource):
 
     @customer_ns.doc('get_customer_list')
     @customer_ns.response(500, 'Internal server error')
-    @customer_ns.marshal_list_with(model)
+    @customer_ns.marshal_list_with(customer_output_model)
     def get(self):
         customer_list = None
         try:
@@ -47,12 +54,11 @@ class CustomerListAPI(Resource):
     @customer_ns.response(200, 'Success')
     @customer_ns.response(409, 'The resource already exists')
     @customer_ns.response(500, 'Internal server error')
-    @customer_ns.expect(model, validate=True)
+    @customer_ns.expect(customer_input_model, validate=True)
     def post(self):
         new_customer = Customer(customer_mail_address=request.json['mail_address'],
                                 customer_first_name=request.json['first_name'],
                                 customer_last_name=request.json['last_name'])
-        '''Surround with try and catch!!'''
         new_customer.set_password(request.json['pin'])
         print(new_customer.customer_pin_hash)
         db.session.add(new_customer)
@@ -70,13 +76,13 @@ class CustomerListAPI(Resource):
 class CustomerAPI(Resource):
 
     @customer_ns.doc('get_customer')
-    @customer_ns.marshal_with(model)
+    @customer_ns.marshal_with(customer_output_model)
     def get(self, id):
         return Customer.query.filter_by(customer_mail_address=id).first()
 
     @customer_ns.doc('del_customer')
-    @customer_ns.marshal_with(model)
+    @customer_ns.response(204, description='No content')
     def delete(self, id):
         db.session.delete(Customer.query.filter_by(customer_mail_address=id).first())
         db.session.commit()
-        return
+        return 204
